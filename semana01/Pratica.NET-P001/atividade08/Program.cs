61b48136dfa5138266647b16e7830770587df9f8
danilo775 <danilo.uba@hotmail.com>
2023-11-17 23:40:13 -0300

    atividade 8

--- semana01/Pratica.NET-P001/atividade08/Program.cs
+++ semana01/Pratica.NET-P001/atividade08/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int num1 = 7;
+int num2 = 3;
+int num3 = 10;
+
+// Verifica se num1 é maior do que num2
+bool maior = num1 > num2;
+
+// Calcula o valor de num1 + num2
+int soma = num1 + num2;
+
+// Verifica se num3 é igual a num1 + num2
+bool igual = num3 == soma;
+
+// Exibe os resultados
+Console.WriteLine("num1 é maior do que num2: {0}", maior);
+Console.WriteLine("num3 é igual a num1 + num2: {0}", igual);
