be53aea238d2d248097d12ac14236e06463c6149
danilo775 <danilo.uba@hotmail.com>
2023-11-17 23:29:04 -0300

    atividade 6

--- semana01/Pratica.NET-P001/atividade06/Program.cs
+++ semana01/Pratica.NET-P001/atividade06/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string str1 = "Hello";
+string str2 = "World";
+
+// Verifica se as duas strings são iguais
+bool iguais = str1 == str2;
+
+// Exibe o resultado
+Console.WriteLine("As strings são iguais: {0}", iguais);
