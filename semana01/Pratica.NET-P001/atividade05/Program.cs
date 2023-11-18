080cdc6a6f44cae0b60fe4992c40e1840dacaec6
danilo775 <danilo.uba@hotmail.com>
2023-11-17 23:28:35 -0300

    atividade 5

--- semana01/Pratica.NET-P001/atividade05/Program.cs
+++ semana01/Pratica.NET-P001/atividade05/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+int b;
+      
+        Console.Write("Digite o valor de 'a': ");
+         a = Convert.ToInt32(Console.ReadLine());
+
+       
+        Console.Write("Digite o valor de 'b': ");
+         b = Convert.ToInt32(Console.ReadLine());
+
+        // Verifica se 'a' é maior que 'b'
+        bool resultado = (a > b);
+
+        // Exibe o resultado
+        Console.WriteLine($"O valor de 'a' ({a}) é maior que o valor de 'b' ({b})? {resultado}");
+
+        Console.ReadLine();
+
