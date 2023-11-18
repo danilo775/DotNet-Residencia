17515d1168712da16b15a4348935ef420690ab6d
danilo775 <danilo.uba@hotmail.com>
2023-11-17 23:32:48 -0300

    atividade 7

--- semana01/Pratica.NET-P001/atividade07/Program.cs
+++ semana01/Pratica.NET-P001/atividade07/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+bool condicao1 = true;
+bool condicao2 = false;
+
+// Verifica se ambas as condições são verdadeiras
+bool ambasVerdadeiras = condicao1 && condicao2;
+
+// Exibe o resultado
+Console.WriteLine("Ambas as condições são verdadeiras: {0}", ambasVerdadeiras);
