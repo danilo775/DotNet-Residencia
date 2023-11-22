f59e266f1270d1ced23b1c1a90b671d27a108527
danilo775 <danilo.uba@hotmail.com>
2023-11-22 13:24:22 -0300

    atv aula sema 03

---
+++ semana03/exercicio01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Data.Common;
+
+
+
+List<(int id, string name, DateTime data)> list =new();
+list.Add((11, "Danilo", new DateTime(2020,10,12)));
+list.Add((12, "Aninha", DateTime.Now));
+list.Add((13, "Juca", DateTime.Now));
+
+list.ForEach(x => Console.WriteLine($"{x.id} - {x.name} - {x.data}"));
+
+
+
+List<string> list1 = new List<string>() { "Danilo", "Aninha", "Juca" };
+
+Console.WriteLine(list1.Any(x => x.Contains("a")));
+
---
+++ semana03/pessoa/Pessoa.cs
@@ -0,0 +1,6 @@
+namespace Namespace;
+public class Pessoa
+{
+    public string Name { get; set; }
+    public float altura { get; set; } = 0.0f;
+}
