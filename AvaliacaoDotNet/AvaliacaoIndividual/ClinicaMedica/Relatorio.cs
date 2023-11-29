84e4ec09f67875da59404a8ac5452e826502ad9b
danilo775 <danilo.uba@hotmail.com>
2023-11-29 11:37:29 -0300

    criado metodo para consulrio

--- AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/Relatorio.cs
+++ AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/Relatorio.cs
@@ -1,12 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace GerenciamentoConsultorio
 {
     static class Relatorios
     {
         public static List<Medico> MedicosComIdadeEntre(List<Medico> medicos, int idadeMinima, int idadeMaxima)
         {
+            return medicos.Where(medico =>
+                CalculaIdade(medico.DataNascimento) >= idadeMinima &&
+                CalculaIdade(medico.DataNascimento) <= idadeMaxima).ToList();
+        }
 
-        }       
-
-
-    } 
-}           
+    
+    }
+}
--- AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/consultorio.cs
+++ AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/consultorio.cs
@@ -11,6 +11,32 @@ namespace GerenciamentoConsultorio
             pacientes = new List<Paciente>();
         }
 
-    }
+        public void AdicionarMedico(Medico medico)
+        {
+            if (!medicos.Exists(m => m.CPF == medico.CPF || m.CRM == medico.CRM))
+            {
+                medicos.Add(medico);
+                Console.WriteLine("Médico adicionado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("CPF ou CRM já existente!");
+            }
+        }
 
-}      
+        public void AdicionarPaciente(Paciente paciente)
+        {
+            if (!pacientes.Exists(p => p.CPF == paciente.CPF))
+            {
+                pacientes.Add(paciente);
+                Console.WriteLine("Paciente adicionado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("CPF já existente!");
+            }
+        }
+
+        // Outros métodos e funcionalidades do consultório...
+    }
+}
