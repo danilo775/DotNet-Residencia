3a339048c0b30168581827a78eed803c16836edf
danilo775 <danilo.uba@hotmail.com>
2023-11-28 22:13:16 -0300

    Pratica DotNet P0003 Gerenciamento Estoque

---
+++ semana03/Pratica.NET-P003/GerenciamentoEstoque/Estoque.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GerenciamentoEstoque
+{
+    public class Estoque
+    {
+        private List<Produto> produtos;
+
+        public Estoque()
+        {
+            produtos = new List<Produto>();
+        }
+
+     
+        public void CadastrarProduto(int codigo, string nome, int quantidade, decimal precoUnitario)
+        {
+            
+            if (produtos.Any(p => p.Codigo == codigo))
+            {
+                throw new Exception("Produto com esse código já cadastrado.");
+            }
+
+        
+            Produto produto = new Produto
+            {
+                Codigo = codigo,
+                Nome = nome,
+                Quantidade = quantidade,
+                PrecoUnitario = precoUnitario
+            };
+
+            produtos.Add(produto);
+        }
+
+       
+        public Produto BuscarProduto(int codigo)
+        {
+            Produto produto = produtos.FirstOrDefault(p => p.Codigo == codigo);
+
+            if (produto == null)
+            {
+                throw new Exception("Produto não encontrado.");
+            }
+
+            return produto;
+        }
+
+     
+        public void AtualizarEstoque(int codigo, int quantidade)
+        {
+            Produto produto = BuscarProduto(codigo);
+
+      
+            if (quantidade > produto.Quantidade)
+            {
+                throw new Exception("Quantidade insuficiente em estoque.");
+            }
+
+            produto.Quantidade += quantidade;
+        }
+        public void GerarRelatorios(int limiteEstoque, decimal minValor, decimal maxValor)
+        {
+        
+            var produtosAbaixoLimite = produtos.Where(p => p.Quantidade < limiteEstoque).ToList();
+
+
+            var produtosEntreValores = produtos.Where(p => p.PrecoUnitario >= minValor && p.PrecoUnitario <= maxValor).ToList();
+
+      
+            decimal valorTotalEstoque = produtos.Sum(p => p.Quantidade * p.PrecoUnitario);
+
+            var valorTotalPorProduto = produtos.ToDictionary(p => p.Nome, p => p.Quantidade * p.PrecoUnitario);
+
+            Console.WriteLine("Lista de produtos com quantidade em estoque abaixo do limite informado:");
+            foreach (var produto in produtosAbaixoLimite)
+            {
+                Console.WriteLine($"{produto.Codigo} - {produto.Nome}");
+            }
+
+            Console.WriteLine("\nLista de produtos com valor entre o mínimo e o máximo informados:");
+            foreach (var produto in produtosEntreValores)
+            {
+                Console.WriteLine($"{produto.Codigo} - {produto.Nome}");
+            }
+
+            Console.WriteLine($"\nValor total do estoque: {valorTotalEstoque}");
+
+            Console.WriteLine("\nValor total de cada produto de acordo com seu estoque:");
+            foreach (var produto in valorTotalPorProduto)
+            {
+                Console.WriteLine($"{produto.Key} - {produto.Value}");
+            }
+        }
+        
+    }
+}
---
+++ semana03/Pratica.NET-P003/GerenciamentoEstoque/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace GerenciamentoEstoque
+{
+    class Program
+    {
+        static Estoque estoque;
+
+        static void Main(string[] args)
+        {
+            estoque = new Estoque();
+
+            bool sair = false;
+
+            while (!sair)
+            {
+                Console.WriteLine("----- MENU -----");
+                Console.WriteLine("1 - Cadastrar Produto");
+                Console.WriteLine("2 - Consultar Produto");
+                Console.WriteLine("3 - Atualizar Estoque");
+                Console.WriteLine("4 - Gerar Relatórios");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("----------------");
+
+                Console.Write("Escolha uma opção: ");
+                int opcao = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (opcao)
+                {
+                    case 1:
+                        CadastrarProduto();
+                        break;
+                    case 2:
+                        ConsultarProduto();
+                        break;
+                    case 3:
+                        AtualizarEstoque();
+                        break;
+                    case 4:
+                        GerarRelatorios();
+                        break;
+                    case 0:
+                        sair = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void CadastrarProduto()
+        {
+            try
+            {
+                Console.Write("Digite o código do produto: ");
+                int codigo = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite a quantidade em estoque: ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o preço unitário do produto: ");
+                decimal preco = decimal.Parse(Console.ReadLine());
+
+                estoque.CadastrarProduto(codigo, nome, quantidade, preco);
+                Console.WriteLine("Produto cadastrado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao cadastrar o produto: {ex.Message}");
+            }
+        }
+
+        static void ConsultarProduto()
+        {
+            try
+            {
+                Console.Write("Digite o código do produto: ");
+                int codigo = int.Parse(Console.ReadLine());
+
+                Produto produto = estoque.BuscarProduto(codigo);
+                Console.WriteLine($"Produto encontrado: {produto.Nome} - Quantidade: {produto.Quantidade} - Preço Unitário: {produto.PrecoUnitario}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao consultar o produto: {ex.Message}");
+            }
+        }
+
+        static void AtualizarEstoque()
+        {
+            try
+            {
+                Console.Write("Digite o código do produto: ");
+                int codigo = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite a quantidade a ser adicionada (valor positivo) ou removida (valor negativo): ");
+                int quantidade = int.Parse(Console.ReadLine());
+
+                estoque.AtualizarEstoque(codigo, quantidade);
+                Console.WriteLine("Estoque atualizado com sucesso.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao atualizar o estoque: {ex.Message}");
+            }
+        }
+        static void GerarRelatorios()
+        {
+            Console.Write("Digite o limite de quantidade em estoque para o relatório de produtos abaixo do limite: ");
+            int limiteEstoque = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor mínimo para o relatório de produtos entre valores: ");
+            decimal minValor = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor máximo para o relatório de produtos entre valores: ");
+            decimal maxValor = decimal.Parse(Console.ReadLine());
+
+            estoque.GerarRelatorios(limiteEstoque, minValor, maxValor);
+        }
+
+       
+    }
+}
