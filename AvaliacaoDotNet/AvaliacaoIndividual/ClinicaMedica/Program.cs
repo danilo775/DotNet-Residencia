811f3ed256742568405a14b5afb46a0bead9a6b1
danilo775 <danilo.uba@hotmail.com>
2023-11-29 12:10:37 -0300

    testando o programa

--- AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/Program.cs
+++ AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             List<Medico> medicos = new List<Medico>
             {
                 new Medico { Nome = "João", DataNascimento = new DateTime(1980, 5, 10), CPF = "12345678901", CRM = "12345" },
@@ -18,6 +19,48 @@
                 new Paciente { Nome = "Mariana", DataNascimento = new DateTime(1998, 7, 12), CPF = "67890123456", Sexo = "Feminino", Sintomas = "Dor de garganta" }
             };
 
+            List<Medico> medicosComIdadeEntre = Relatorios.MedicosComIdadeEntre(medicos, 30, 50);
+            Console.WriteLine("Médicos com idade entre 30 e 50 anos:");
+            foreach (var medico in medicosComIdadeEntre)
+            {
+                Console.WriteLine(medico.Nome);
+            }
+
+            Console.WriteLine();
+
+            List<Paciente> pacientesComIdadeEntre = Relatorios.PacientesComIdadeEntre(pacientes, 20, 30);
+            Console.WriteLine("Pacientes com idade entre 20 e 30 anos:");
+            foreach (var paciente in pacientesComIdadeEntre)
+            {
+                Console.WriteLine(paciente.Nome);
+            }
+
+            Console.WriteLine();
+
+            List<Paciente> pacientesPorSexo = Relatorios.PacientesPorSexo(pacientes, "Feminino");
+            Console.WriteLine("Pacientes do sexo feminino:");
+            foreach (var paciente in pacientesPorSexo)
+            {
+                Console.WriteLine(paciente.Nome);
+            }
+
+            Console.WriteLine();
+
+            List<Paciente> pacientesEmOrdemAlfabetica = Relatorios.PacientesEmOrdemAlfabetica(pacientes);
+            Console.WriteLine("Pacientes em ordem alfabética:");
+            foreach (var paciente in pacientesEmOrdemAlfabetica)
+            {
+                Console.WriteLine(paciente.Nome);
+            }
+
+            Console.WriteLine();
+
+            List<Paciente> pacientesPorSintoma = Relatorios.PacientesPorSintoma(pacientes, "Dor");
+            Console.WriteLine("Pacientes com sintoma de dor:");
+            foreach (var paciente in pacientesPorSintoma)
+            {
+                Console.WriteLine(paciente.Nome);
+            }
         }
     }
 }
--- AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/Relatorio.cs
+++ AvaliacaoDotNet/AvaliacaoIndividual/ClinicaMedica/Relatorio.cs
@@ -18,5 +18,33 @@ namespace GerenciamentoConsultorio
                 idade--;
             return idade;
         }
+
+        public static List<Paciente> PacientesComIdadeEntre(List<Paciente> pacientes, int idadeMinima, int idadeMaxima)
+        {
+            return pacientes.Where(paciente =>
+                CalculaIdade(paciente.DataNascimento) >= idadeMinima &&
+                CalculaIdade(paciente.DataNascimento) <= idadeMaxima).ToList();
+        }
+
+        public static List<Paciente> PacientesPorSexo(List<Paciente> pacientes, string sexo)
+        {
+            return pacientes.Where(paciente => paciente.Sexo.Equals(sexo)).ToList();
+        }
+
+        public static List<Paciente> PacientesEmOrdemAlfabetica(List<Paciente> pacientes)
+        {
+            return pacientes.OrderBy(paciente => paciente.Nome).ToList();
+        }
+
+        public static List<Paciente> PacientesPorSintoma(List<Paciente> pacientes, string sintoma)
+        {
+            return pacientes.Where(paciente => paciente.Sintomas.Contains(sintoma)).ToList();
+        }
+
+        public static List<Pessoa> AniversariantesDoMes(List<Medico> medicos, List<Paciente> pacientes, int mes)
+        {
+            return medicos.Cast<Pessoa>().Concat(pacientes.Cast<Pessoa>())
+                .Where(pessoa => pessoa.DataNascimento.Month == mes).ToList();
+        }
     }
 }
